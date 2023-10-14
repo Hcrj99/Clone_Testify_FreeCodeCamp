28bdbedfd702c6b120f98e6c32bc928efb0ac2de
hcrj99 <hcrj99@hotmail.com>
2023-10-14 18:34:23 -0500

    use of props

--- src/components/Testify.js
+++ src/components/Testify.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import '../Styles/Testify.css'
 
-function Testify(){
+function Testify(props){
   return (
     <section className='container__testify'>
-      <img className='container__testify-image' src={require('../images/testify_emma.png')} alt='Photogaphy' />
+      <img className='container__testify-image' src={require(`../images/testify_${props.image}.png`)} alt='Photogaphy' />
       <div className='container__testify-text'>
-        <p className='testify-text__title'>Emma Bostian in Sweden</p>
-        <p className='testify-text__position'>Software Engineer at Spotify</p>
-        <p className='testify-text__testify'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+        <p className='testify-text__title'>
+          <strong>{props.name}</strong> in {props.country}
+        </p>
+        <p className='testify-text__position'>
+          {props.work} at <strong>{props.company}</strong>
+        </p>
+        <p className='testify-text__testify'>"{props.testify}"</p>
       </div>
     </section>
   );
