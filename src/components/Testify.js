c0d1442da834b87e3507c022d46c3ca59484f7c2
hcrj99 <hcrj99@hotmail.com>
2023-10-14 13:10:58 -0500

    Initial struct of the component Testify

---
+++ src/components/Testify.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Testify(){
+  return (
+    <section className='container__testify'>
+      <img className='container__testify-image' src={require('../images/testify_emma.png')} alt='Photo'/>
+
+    </section>
+  );
+}
